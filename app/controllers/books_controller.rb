f37178cd501088f159383287a89b444be09e3bf2
nadinesk <nadine.khattak@gmail.com>
2017-09-06 12:18:49 -0400

    added routes and books_conroller and jbuilder views

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,84 @@
+class Api::V1::BooksController < ApplicationController
+
+
+
+   def index
+      @books = Book.all
+    	render 'books/books.json.jbuilder', books: @books
+   end
+
+
+   def show
+   		@book = Book.find_by(id: params[:id])
+   		if @book
+   			render 'books/book.json.jbuilder', book: @book
+   		else
+   			render json: {
+   				errors: {
+   					message: "Page not found"
+   				}
+   			}, status: 404
+   		end
+
+   end
+
+   def create
+   			@book = Book.new(book_params)
+   			if @book.save
+   				render '/books/book.json.jbuilder', book: @book
+   			else
+   				render json: {
+   					errors: @book.errors.full_messages
+   				}, status: 500
+   			end
+   		else
+   			render json: {
+   				errors: [
+   					{message: "No permission to add books"}
+   				]
+   			}, status: 403
+   		end
+
+   end
+
+   def update
+   			@book = Book.find_by(id: params[:id])
+   			if @book.update_attributes(book_params)
+   				render 'books/book.json.jbuilder', book: @book
+   			else
+   				render json: {
+   					errors: @book.errors.full_messages
+   				}, status: 500
+   			end
+   		else
+   			render json: {
+   				errors: [
+   					{message: "You don't have permission to edit this book"}
+   				]
+   			}, status: 403
+   		end
+   end
+
+   def destroy
+   		book = Book.find_by(id: params[:id])
+   		if book
+   			book.destroy
+   			head :no_contnet
+   		else
+   			render json: {
+   				errors: {
+   					message: "Page not found"
+   				}
+   			}, status: 404
+   		end
+
+   end
+
+  private
+
+    def book_params
+      params.require(:book).permit(:title, :author, :rating, :notes)
+    end
+
+
+end
---
+++ spec/factories/books.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :book do
+    title "MyString"
+    author "MyString"
+    rating "MyString"
+    notes "MyString"
+  end
+end
