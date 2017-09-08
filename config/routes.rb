bf19a85c98c8bbdc9d910635e5aa69d0ee25a360
nadinesk <nadine.khattak@gmail.com>
2017-09-08 18:59:16 -0400

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   namespace :api do 
     namespace :v1 do  
         root "books#ping"
-        resources :books
+        resources :books, only: [:index, :create, :update, :delete, :show, :ping]
     end
   end
 end
