4ca12618fe57605ceb44493a93aab335817494a6
nadinesk <nadine.khattak@gmail.com>
2017-09-13 22:37:21 -0400

    fixed migrations

--- db/schema.rb
+++ db/schema.rb
@@ -22,18 +22,7 @@ ActiveRecord::Schema.define(version: 20170914021041) do
     t.string "notes"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "user_id"
     t.integer "likes", default: 0
-    t.index ["user_id"], name: "index_books_on_user_id"
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "password_digest"
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  add_foreign_key "books", "users"
 end
