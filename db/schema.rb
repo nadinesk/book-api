01bc91bb04cb45eef22d99150c00fbdbef42567f
nadinesk <nadine.khattak@gmail.com>
2017-09-13 22:11:40 -0400

    added default value to books likes column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170914004348) do
+ActiveRecord::Schema.define(version: 20170914021041) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,7 +23,7 @@ ActiveRecord::Schema.define(version: 20170914004348) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
-    t.integer "likes"
+    t.integer "likes", default: 0
     t.index ["user_id"], name: "index_books_on_user_id"
   end
 
